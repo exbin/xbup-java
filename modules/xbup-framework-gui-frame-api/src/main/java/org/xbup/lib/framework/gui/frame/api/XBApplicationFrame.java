539814523c378a885e65670e28ddd85428a6199f
hajdam <hajdam@users.sf.net>
2015-10-29 23:50:16 +0100

    GUI Reworking - 9: Preparation for GUI framework 5

--- modules/xbup-framework-gui-frame-api/src/main/java/org/xbup/lib/framework/gui/frame/api/XBApplicationFrame.java
+++ modules/xbup-framework-gui-frame-api/src/main/java/org/xbup/lib/framework/gui/frame/api/XBApplicationFrame.java
@@ -14,20 +14,20 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along this application.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.xbup.lib.framework.gui.menu.api;
+package org.xbup.lib.framework.gui.frame.api;
 
 /**
- * Interface for application's panel.
+ * Interface for editor frame.
  *
- * @version 0.2.0 2015/10/25
+ * @version 0.1.23 2013/09/26
  * @author XBUP Project (http://xbup.org)
  */
-public interface ApplicationPanel {
+public interface XBApplicationFrame {
 
     /**
-     * Gets panel name.
+     * Returns main frame management.
      *
-     * @return panel name
+     * @return main frame management
      */
-    public String getPanelName();
+    //public MainFrameManagement getMainFrameManagement();
 }

2df8cf953a91763c102056a996487d53ed09c78a
hajdam <hajdam@users.sf.net>
2015-11-06 23:47:32 +0100

    GUI Reworking - 17: Added some more packages

--- modules/xbup-framework-gui-menu/src/main/java/org/xbup/lib/framework/gui/undo/ApplicationPanel.java
+++ modules/xbup-framework-gui-menu/src/main/java/org/xbup/lib/framework/gui/undo/ApplicationPanel.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along this application.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.xbup.lib.framework.gui.menu;
+package org.xbup.lib.framework.gui.undo;
 
 /**
  * Interface for application's panel.

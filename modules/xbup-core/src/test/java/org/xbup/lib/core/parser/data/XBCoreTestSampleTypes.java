2788a4b6f1c5b20150bd9f06d022e0021f9a1348
hajdam <hajdam@users.sf.net>
2015-11-23 23:51:39 +0100

    Types tests - Preparation 2

---
+++ modules/xbup-core/src/test/java/org/xbup/lib/core/parser/data/XBCoreTestSampleTypes.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) XBUP Project
+ *
+ * This application or library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This application or library is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along this application.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.xbup.lib.core.parser.data;
+
+/**
+ * Sample data and methods for testing purposes.
+ *
+ * @version 0.2.0 2015/11/23
+ * @author XBUP Project (http://xbup.org)
+ */
+public class XBCoreTestSampleTypes {
+
+    public final static String SAMPLE_TYPES_PATH = "/org/xbup/lib/core/resources/test/samples/types/";
+
+    // Undefined types
+    public final static String SAMPLE_UNDEFINED_TYPES_PATH = SAMPLE_TYPES_PATH + "undefined/";
+    public final static String SAMPLE_UNDEFINED_STRING = SAMPLE_UNDEFINED_TYPES_PATH + "string.xb";
+    public final static String SAMPLE_UNDEFINED_STRING_TERMINATED = SAMPLE_UNDEFINED_TYPES_PATH + "string_terminated.xb";
+}

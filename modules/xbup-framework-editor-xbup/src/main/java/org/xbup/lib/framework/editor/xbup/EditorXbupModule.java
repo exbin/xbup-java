539b3ca967a8a4d6e462dcfb6d32f74ac15ea2c8
hajdam <hajdam@users.sf.net>
2015-12-17 23:38:24 +0100

    XBEditor using new framework - 13: Lazy day

---
+++ modules/xbup-framework-editor-xbup/src/main/java/org/xbup/lib/framework/editor/xbup/EditorXbupModule.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) XBUP Project
+ *
+ * This application or library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This application or library is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along this application.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.xbup.lib.framework.editor.xbup;
+
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+import org.xbup.lib.framework.gui.api.XBApplication;
+import org.xbup.lib.framework.gui.api.XBApplicationModulePlugin;
+
+/**
+ * XBUP editor module.
+ *
+ * @version 0.2.0 2015/12/17
+ * @author XBUP Project (http://xbup.org)
+ */
+@PluginImplementation
+public class EditorXbupModule implements XBApplicationModulePlugin {
+
+    private XBApplication application;
+
+    public EditorXbupModule() {
+    }
+
+    @Override
+    public void init(XBApplication application) {
+        this.application = application;
+    }
+
+    @Override
+    public void unregisterPlugin(String pluginId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}

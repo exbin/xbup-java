794192fab850aba423b9e56289451a2b4eca6965
hajdam <hajdam@users.sf.net>
2015-12-27 23:26:54 +0100

    XBEditor using new framework - 23: Lazy day

--- modules/org-xbup-framework-gui-api/src/main/java/org/xbup/lib/framework/gui/api/XBModuleRepositoryUtils.java
+++ modules/org-xbup-framework-gui-api/src/main/java/org/xbup/lib/framework/gui/api/XBModuleRepositoryUtils.java
@@ -13,6 +13,9 @@ package org.xbup.lib.framework.gui.api;
  */
 public class XBModuleRepositoryUtils {
 
+    private static final String API_CLASS_EXT = "Api";
+    private static final String API_PACKAGE_EXT = ".api";
+
     /**
      * Returns module identifier for class representation.
      *
@@ -23,7 +26,7 @@ public class XBModuleRepositoryUtils {
         String packageName = moduleClass.getPackage().getName();
         String className = moduleClass.getSimpleName();
 
-        return (packageName.endsWith(".api") ? packageName.substring(0, packageName.length() - 3) : packageName)
-                + (className.endsWith("Api") ? className.substring(0, className.length() - 3) : className);
+        return (packageName.endsWith(API_PACKAGE_EXT) ? packageName.substring(0, packageName.length() - 3) : packageName)
+                + (className.endsWith(API_CLASS_EXT) ? className.substring(0, className.length() - 3) : className);
     }
 }
